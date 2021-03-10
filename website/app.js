6f629a8830737bc5d6970faecb93a691d88a8c21
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2021-03-10 16:08:28 +0200

    Set global vairiables for weather api

--- website/app.js
+++ website/app.js
@@ -1,4 +1,6 @@
 /* Global Variables */
+const baseURL = 'api.openweathermap.org/data/2.5/weather?';
+const apiKey = '&appid=6684c2778e3d4aefc3f8ba10eafc527c';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
