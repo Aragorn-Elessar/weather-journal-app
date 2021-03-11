089d7e0953c84378045b4de7f24be81b0dffcb22
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2021-03-11 14:00:10 +0200

    Add function to handle data posted to server

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-const baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+const units = '&units=metric';
 const apiKey = '&appid=6684c2778e3d4aefc3f8ba10eafc527c';
 
 // Listen for generate btn and perform data collection
@@ -8,19 +9,20 @@ document.getElementById('generate').addEventListener('click', performAction);
 // Grab zip code user entered 
 function performAction() {
     const zipCode = document.getElementById('zip').value;
-    // Check if user entered zip
-    if (zipCode) {
-    // Call getWeather and pass it your url credentials
-    getWeather(baseURL, zipCode, apiKey)
+    // Check if user entered a valid zip
+    if (zipCode && zipCode.length == 5) {
+    getWeather(baseURL, zipCode, units, apiKey)
+    // .then(postData(data))
     } else {
         alert('Yoink!');
     }
 }
 
 // Get weather data
-const getWeather = async (URL, zip, key) => {
-    console.log(URL+zip+key);
-    const res = await fetch(URL+zip+key);
+const getWeather = async (URL, zip, unit, key) => {
+    console.log(URL+zip+unit+key);
+    const res = await fetch(URL+zip+unit+key);
+    console.log(res);
 
     try {
         const data = await res.json();
@@ -31,6 +33,28 @@ const getWeather = async (URL, zip, key) => {
     }
 }
 
+// Post data to server
+const postData = async (url='', data = {}) => {
+    
+    await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        console.log(data);
+        return;
+    } catch(error) {
+        console.log(`error: ${error}`);
+    }
+}
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+console.log(newDate);
